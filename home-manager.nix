a002401a02e965924a7da180d2692dbbd02bd04f
Tomás Xavier Santos <oipxtomas@gmail.com>
2025-11-06 01:47:50 -0300

    Adds fetchTarball configuration into home-manager settings
    to comply with flakes needs, this adds sha256 verification to
    the fetch tarball call

--- home-manager.nix
+++ home-manager.nix
@@ -1,7 +1,11 @@
 { config, pkgs, lib, ... }:
 
 let
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+  home-manager = (builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz"; 
+    sha256 = "0q3lv288xlzxczh6lc5lcw0zj9qskvjw3pzsrgvdh8rl8ibyq75s";
+    }
+);
 in
 {
   imports =
